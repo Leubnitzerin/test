f2680ad04a9f7d02c107f64db22e7c7b7bfbb96f
Leubnitzerin <132013779+Leubnitzerin@users.noreply.github.com>
2023-04-27 22:15:26 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import datetime
+#from datetime import date
+import numpy as np
+import pandas as pd
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+
+aktuellesDatum = datetime.datetime.today()
+print(aktuellesDatum)
+
+start = datetime.datetime(2021,2,5) 
+end = datetime.datetime.today()
+print(end)
+apple = web.DataReader("BNTX", "yahoo",end)
+print(apple)
+apple = web.DataReader("5CV.DE", "yahoo",end)
+print(apple)
+
+
